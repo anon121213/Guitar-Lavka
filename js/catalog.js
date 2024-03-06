88d84c74965e38df87d2df932c443bec3eb02c7a
anon121213 <anon121213@mail.ru>
2024-03-07 00:45:22 +0300

    catalog

--- js/catalog.js
+++ js/catalog.js
@@ -48,8 +48,8 @@ const gridActive = document.querySelector('.grid_Active')
 const gridNonActive = document.querySelector('.grid_nonActive') 
 const listActive = document.querySelector('.list_Active')
 const listNonActive = document.querySelector('.list_nonActive')
-
-let grid = true
+const list = document.getElementById('list')
+const grid = document.getElementById('grid')
 
 gridNonActive.addEventListener('click', e => {
 
@@ -57,17 +57,21 @@ gridNonActive.addEventListener('click', e => {
     gridNonActive.classList.add('hidden')
     listActive.classList.add('hidden')
     listNonActive.classList.remove('hidden')
-    grid = true
+    grid.classList.remove('hidden')
+    list.classList.add('hidden')
 
 })
 
 listNonActive.addEventListener('click', e => {
 
+    console.log(list)
+    console.log(grid)
     gridActive.classList.add('hidden')
     gridNonActive.classList.remove('hidden')
     listActive.classList.remove('hidden')
     listNonActive.classList.add('hidden')
-    grid = false
+    grid.classList.add('hidden')
+    list.classList.remove('hidden')
 
 })
 
@@ -77,36 +81,93 @@ listNonActive.addEventListener('click', e => {
 const nums = document.querySelectorAll('.nums')
 const last = document.querySelector('.last')
 const first = document.querySelector('.firstElement')
+const start = document.querySelector('.start')
+const finish = document.querySelector('.finish')
+let numsId = null
+
+start.addEventListener('click', event => {
+
+    nums.forEach(items => items.classList.remove('box'))
+
+    if(numsId < 2){
+        numsId = nums.length + 1
+    }
+
+    nums[numsId - 2].classList.add('box')
+    numsId--
+
+})
+
+finish.addEventListener('click', event => {
+
+    nums.forEach(items => items.classList.remove('box'))
+
+    if(numsId > nums.length - 1){
+        numsId = 0
+    }
+
+    nums[numsId].classList.add('box')
+    numsId++
+
+})
 
 nums.forEach(item => item.addEventListener('click', event => {
 
     nums.forEach(items => items.classList.remove('box'))
-
     event.target.classList.add('box')
+    numsId = event.target.id
 
 }))
 
 last.addEventListener('click', event => {
 
-    const lastel = nums.length - 1
-    
+    const lastel = nums.length - 1    
     nums.forEach(items => items.classList.remove('box'))
-
     nums[lastel].classList.add('box')
-
-    console.log('asfdasf')
+    numsId = nums.length
 
 })
 
 first.addEventListener('click', event => {
     
-    console.log('asfdasf')
-
     nums.forEach(items => items.classList.remove('box'))
-
     nums[0].classList.add('box')
+    numsId = 0
 
 })
+
+// favorites button
+
+const nonActiveHeart = document.querySelectorAll('.nonActiveHeart')
+const ActiveHeart = document.querySelectorAll('.ActiveHeart')
+heartsId = null
+
+nonActiveHeart.forEach(items => items.addEventListener('click', event => {
+    heartsId = event.target.id
+    nonActiveHeart[heartsId - 1].classList.add('hidden')
+    ActiveHeart[heartsId - 1].classList.remove('hidden')
+}))
+
+ActiveHeart.forEach(items => items.addEventListener('click', event => {
+    heartsId = event.target.id
+    ActiveHeart[heartsId - 1].classList.add('hidden')
+    nonActiveHeart[heartsId - 1].classList.remove('hidden')
+}))
+
+// buscket button
+
+const ActiveBuscket = document.querySelectorAll('.ActiveBuscket')
+const nonActiveBuscket = document.querySelectorAll('.nonActiveBuscket')
+backetId = null
+
+nonActiveBuscket.forEach(items => items.addEventListener('click', event => {
+    backetId = event.target.id
+    nonActiveBuscket[backetId - 1].classList.add('hidden')
+    ActiveBuscket[backetId - 1].classList.remove('hidden')
+}))
+
+
+
 
 // player
 
@@ -121,29 +182,29 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 //    after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    'autoplay': 0,
-    'disablekb': 0,   
-    videoId: 'z0DnBiM3DtQ',
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
+    player = new YT.Player('player', {
+        'autoplay': 0,
+        'disablekb': 0,   
+        videoId: 'z0DnBiM3DtQ',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
 }
 
 function onPlayerReady(event) {
-  event.target.playVideo();
+    event.target.playVideo();
 }
 
 var done = false;
 function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
 }
 function stopVideo() {
-  player.stopVideo();
+    player.stopVideo();
 }
 
