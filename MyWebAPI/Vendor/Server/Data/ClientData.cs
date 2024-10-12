8082022e77a688ff2728c2de1828ce1cadca5c50
Arthur <anon121213@mail.ru>
2024-10-12 03:13:18 +0300

    [add] isStock checker

---
+++ MyWebAPI/Vendor/Server/Data/ClientData.cs
@@ -0,0 +1,8 @@
+﻿namespace MyWebAPI.Vendor.Server.EventSystem;
+
+public struct ClientData
+{
+    public bool IsStock { get; set; }
+    public int MinPrice { get; set; }
+    public int MaxPrice { get; set; }
+}
